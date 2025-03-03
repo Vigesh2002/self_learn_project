855699416537a85ce9ffbe37986cc4eff6a428a1
Vigesh <robovigesh987654321@gmail.com>
2025-03-04 00:14:53 +0530

    work done

---
+++ TypeScript/Annotations/App.ts
@@ -0,0 +1,15 @@
+// Annotations
+
+// Annotations are used to specify the data type of a variable, parameter, function return value, and other type of values. Annotations help developers catch errors early in development by allowing them to specify what types of values can be assigned to a given variables or passed as an argument to a function.
+
+// String
+let myName: string = "Vigesh";
+console.log(myName);
+
+// Number
+let favNumber: number = 10;
+console.log(favNumber);
+
+// Boolean
+let isHard: boolean = true;
+console.log(isHard);
---
+++ TypeScript/Type Inference/App.ts
@@ -0,0 +1,15 @@
+// Type Inference
+
+// Type inference is a feature in TypeScript that allows the compiler to automatically determine the type of a variable based on its value. In other words, if you declare a variable without explicitly specifying its type, TypeScript will try to infer the based on the value assign to it.
+
+let tech = "TypeScript";
+console.log(tech)
+console.log(typeof tech)
+
+let favNumber = 10;
+console.log(favNumber);
+console.log(typeof favNumber);
+
+let isHard = true;
+console.log(isHard);
+console.log(typeof isHard);
